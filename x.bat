610300e9174d85c21edb416a506337031bed9f4e
arnim noeldechen <arnim.noeldechen@gwtel.de>
2021-05-07 11:29:34 +0200

    remove file

--- x.bat
+++
@@ -1,6 +0,0 @@
-echo "# HandsOn_VSCode_Python" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/anoeldechen/HandsOn_VSCode_Python.git
-git push -u origin
